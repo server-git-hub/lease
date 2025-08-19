6ca6f06e30a7ff57dd540cb87ab8679c5f9d8067
server-git-hub <1225789863@qq.com>
2025-08-19 18:14:24 +0800

    第三次提交，租期管理实现

--- web/web-admin/src/main/java/com/atguigu/lease/web/admin/controller/apartment/LeaseTermController.java
+++ web/web-admin/src/main/java/com/atguigu/lease/web/admin/controller/apartment/LeaseTermController.java
@@ -3,8 +3,10 @@ package com.atguigu.lease.web.admin.controller.apartment;
 
 import com.atguigu.lease.common.result.Result;
 import com.atguigu.lease.model.entity.LeaseTerm;
+import com.atguigu.lease.web.admin.service.LeaseTermService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,21 +16,28 @@ import java.util.List;
 @RestController
 public class LeaseTermController {
 
+    @Autowired
+    private LeaseTermService leaseTermService;
+
+
     @GetMapping("list")
     @Operation(summary = "查询全部租期列表")
     public Result<List<LeaseTerm>> listLeaseTerm() {
-        return Result.ok();
+        List<LeaseTerm> list = leaseTermService.list();
+        return Result.ok(list);
     }
 
     @PostMapping("saveOrUpdate")
     @Operation(summary = "保存或更新租期信息")
     public Result saveOrUpdate(@RequestBody LeaseTerm leaseTerm) {
-        return Result.ok();
+        boolean flag = leaseTermService.saveOrUpdate(leaseTerm);
+        return flag ? Result.ok() : Result.fail();
     }
 
     @DeleteMapping("deleteById")
     @Operation(summary = "根据ID删除租期")
     public Result deleteLeaseTermById(@RequestParam Long id) {
-        return Result.ok();
+        boolean flag = leaseTermService.removeById(id);
+        return flag ? Result.ok() : Result.fail();
     }
 }

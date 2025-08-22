b304600be7da01d77b5d6c63afa5892c598056fa
server-git-hub <1225789863@qq.com>
2025-08-22 23:08:50 +0800

    第十一次提交，用户模块接口实现

--- web/web-admin/src/main/java/com/atguigu/lease/web/admin/controller/user/UserInfoController.java
+++ web/web-admin/src/main/java/com/atguigu/lease/web/admin/controller/user/UserInfoController.java
@@ -4,10 +4,15 @@ package com.atguigu.lease.web.admin.controller.user;
 import com.atguigu.lease.common.result.Result;
 import com.atguigu.lease.model.entity.UserInfo;
 import com.atguigu.lease.model.enums.BaseStatus;
+import com.atguigu.lease.web.admin.service.UserInfoService;
 import com.atguigu.lease.web.admin.vo.user.UserInfoQueryVo;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "用户信息管理")
@@ -15,15 +20,27 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/admin/user")
 public class UserInfoController {
 
+    @Autowired
+    private UserInfoService userInfoService;
+
     @Operation(summary = "分页查询用户信息")
     @GetMapping("page")
     public Result<IPage<UserInfo>> pageUserInfo(@RequestParam long current, @RequestParam long size, UserInfoQueryVo queryVo) {
-        return Result.ok();
+        IPage<UserInfo> page=new Page<>(current,size);
+        LambdaQueryWrapper<UserInfo> wrapper=new LambdaQueryWrapper<>();
+        wrapper.like(queryVo.getPhone()!=null,UserInfo::getPhone,queryVo.getPhone());
+        wrapper.eq(queryVo.getStatus()!=null,UserInfo::getStatus,queryVo.getStatus());
+        userInfoService.page(page,wrapper);
+        return Result.ok(page);
     }
 
     @Operation(summary = "根据用户id更新账号状态")
     @PostMapping("updateStatusById")
     public Result updateStatusById(@RequestParam Long id, @RequestParam BaseStatus status) {
-        return Result.ok();
+        LambdaUpdateWrapper<UserInfo> updateWrapper = new LambdaUpdateWrapper<>();
+        updateWrapper.eq(UserInfo::getId, id);
+        updateWrapper.set(UserInfo::getStatus, status);
+        boolean flag = userInfoService.update(updateWrapper);
+        return flag ? Result.ok(): Result.fail();
     }
 }
